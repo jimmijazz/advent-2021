ce221a1679dc249e2877360901afdb92d2368793
Josh Bitossi <joshua.bitossi@shopify.com>
2021-12-07 10:05:57 +1000

    day 1 challenge 1

---
+++ 1/1.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+# https://adventofcode.com/2021/day/1
+# Count the number of times a number increases from the previous number
+
+# 1st approach - opening the file
+# file = File.open("data.txt") # Open the text file
+# file_data = file.readlines.map(&:chomp) # Split to new lines and remove new line char
+# file.close # free up memory (can also just use File.read instead)
+
+# Better approach - reading line
+
+$count = 0
+$previousValue
+$currentValue
+
+def updateAndCheckValues(value) 
+    $currentValue = value.to_i # Convert string to number
+
+    # First instance check
+    if $previousValue.nil? 
+        # Skip because no previous value
+        puts "#{value} (n/a) - no previous measurement"
+    elsif $currentValue > $previousValue
+        # Not first instance
+        $count += 1
+        puts " #{value} (increased)"
+    else 
+        puts " #{value} (decreased)"
+    end
+    
+    $previousValue = value.to_i  # Update previous value
+
+end
+
+File.foreach("data.txt") { | line | updateAndCheckValues(line)}
+puts "Final Count: #{$count}"
